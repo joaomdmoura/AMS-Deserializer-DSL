b5c45614ff47e23de7a59ffb20ebf34c6b80935f
João Moura <joaomdmoura@gmail.com>
2015-06-06 16:46:46 -0300

    updating posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.create(post_params)
+    render json: @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    render json: @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params
+    .extend(PostSerializer)
+    .require(:post)
+    .permit(:title, :body)
+  end
 end

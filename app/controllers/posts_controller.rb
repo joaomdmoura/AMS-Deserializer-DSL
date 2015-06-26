ea7ba1290f45762744453267885f2f34068b9eaa
João Moura <joaomdmoura@gmail.com>
2015-06-26 17:51:15 -0300

    updating gems and uncommenting deserialization implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-  # include ActiveModel::Deserializer
+  include ActiveModel::Deserializer
 
   def index
     @posts = Post.all
     render json: @posts
   end
 
-  # def create
-  #   @post = deserialize(:post).object
-  #   @post.save
-  #   render json: @post
-  # end
+  def create
+    @post = deserialize(:post).object
+    @post.save
+    render json: @post
+  end
 end

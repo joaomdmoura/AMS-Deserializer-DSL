80f19f05c2c51c077829a56d375c304b8229b471
João Moura <joaomdmoura@gmail.com>
2015-07-04 03:18:59 -0300

    updating implementation to start supporting adapters convention

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
  private
 
  def post_params
-   params.require(:post).permit(PostSerialization._params)
+   PostSerialization.permit(params)
  end
 end
